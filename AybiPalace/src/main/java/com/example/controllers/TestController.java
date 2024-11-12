582ecf7023916a4dbbfaf341ae089769c6c63ddd
Maria A. <132586217+maria-bg@users.noreply.github.com>
2024-11-12 18:27:33 -0300

    Add files via upload

---
+++ AybiPalace/src/main/java/com/example/controllers/TestController.java
@@ -0,0 +1,18 @@
+package com.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class TestController {
+	
+	@GetMapping("/")
+	public ModelAndView home() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/home");
+		return mv;
+	}
+	
+}
