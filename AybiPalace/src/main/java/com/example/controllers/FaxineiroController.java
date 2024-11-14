8a4c44fd501e74229876e3f1a42f82a1020656a2
Eduardo Alves Lins <140346484+eduardoalins@users.noreply.github.com>
2024-11-13 21:08:35 -0300

    Update FaxineiroController.java

--- AybiPalace/src/main/java/com/example/controllers/FaxineiroController.java
+++ AybiPalace/src/main/java/com/example/controllers/FaxineiroController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.entities.Faxineiro;
 import com.example.repositories.FaxineiroRepository;
@@ -23,10 +24,14 @@ public class FaxineiroController {
     }
 
     @GetMapping
-    public String listarFaxineiros(Model model) {
-        List<Faxineiro> faxineiros = faxineiroRepository.findAll();
+    public String listarFaxineiros(@RequestParam(name = "salarioMin", required = false) Float salarioMin, Model model) {
+        List<Faxineiro> faxineiros;
+        if (salarioMin != null) {
+            faxineiros = faxineiroRepository.findBySalarioGreaterThan(salarioMin);
+        } else {
+            faxineiros = faxineiroRepository.findAll();
+        }
         model.addAttribute("faxineiros", faxineiros);
-        return "faxineiros"; // Nome do arquivo HTML (src/main/resources/templates/faxineiros.html)
+        return "faxineiros";
     }
 }
-
