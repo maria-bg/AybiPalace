6b3b59c0c3bca11ed2adacbd2f4706b26ec83bbf
Eduardo Alves Lins <140346484+eduardoalins@users.noreply.github.com>
2024-11-15 18:23:59 -0300

    Delete AybiPalace/src/main/java/com/example/repositories directory

--- AybiPalace/src/main/java/com/example/repositories/HospedeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.entities.Hospede;
-
-public interface HospedeRepository extends JpaRepository<Hospede, String> {
-}
