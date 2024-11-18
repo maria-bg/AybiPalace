ff20be3d179bb531dcf35658a43e606d30294447
Maria A. <132586217+maria-bg@users.noreply.github.com>
2024-11-18 19:01:26 -0300

    Update TestController.java

--- AybiPalace/src/main/example/controllers/TestController.java
+++ AybiPalace/src/main/example/controllers/TestController.java
@@ -35,4 +35,67 @@ public class TestController {
 		mv.setViewName("/cadastro");
 		return mv;
 	}
+	
+	@GetMapping("/acomodacoes")
+	public ModelAndView acomodacoes() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/acomodacoes");
+		return mv;
+	}
+	
+	@GetMapping("/quarto-ayla")
+	public ModelAndView quartoAyla() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/ayla");
+		return mv;
+	}
+	
+	@GetMapping("/quarto-toby")
+	public ModelAndView quartoToby() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/toby");
+		return mv;
+	}
+	
+	@GetMapping("/quarto-pipoca")
+	public ModelAndView quartoPipoca() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/pipoca");
+		return mv;
+	}
+	
+	@GetMapping("/adm")
+	public ModelAndView administracao() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/administracao");
+		return mv;
+	}
+	
+	@GetMapping("/loginGerente")
+	public ModelAndView loginGerente() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/loginGerente");
+		return mv;
+	}
+	
+	@GetMapping("/loginFaxineiro")
+	public ModelAndView loginFaxineiro() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/loginFaxineiro");
+		return mv;
+	}
+	
+	@GetMapping("/homeGerente")
+	public ModelAndView homeGerente() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/homeGerente");
+		return mv;
+	}
+	
+	@GetMapping("/homeFaxineiro")
+	public ModelAndView homeFaxineiros() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/homeFaxineiro");
+		return mv;
+	}
 }
